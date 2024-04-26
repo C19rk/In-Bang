a5cd9cb5fb37193f838364379fc4552bdf1249f3
C19rk <manuntagclark@gmail.com>
2024-04-26 22:26:47 +0800

    backend

---
+++ static_my_project/precache-manifest.d3e758105038a5f55beb2e59dc73a8f2.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe947aea38cbb7c32531a6a2240989c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7adedbd29738925dd3cf",
+    "url": "/static/css/main.d3236750.chunk.css"
+  },
+  {
+    "revision": "3cc06e7e4c6629427572",
+    "url": "/static/js/2.b516af4f.chunk.js"
+  },
+  {
+    "revision": "f65de02737701642883a5a1149e30e84",
+    "url": "/static/js/2.b516af4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18b532bd62e18539b12",
+    "url": "/static/js/3.6560d225.chunk.js"
+  },
+  {
+    "revision": "7adedbd29738925dd3cf",
+    "url": "/static/js/main.8b7b04c2.chunk.js"
+  },
+  {
+    "revision": "3e0b2295379e5cac1ac2",
+    "url": "/static/js/runtime-main.47f5b2ed.js"
+  },
+  {
+    "revision": "283fd06dadf2ae6d0267c681c0c3c359",
+    "url": "/static/media/ABirdsNest.283fd06d.mp4"
+  },
+  {
+    "revision": "6aabcff4c0ebdcb4fd80e5000923068c",
+    "url": "/static/media/ABirdsNest.6aabcff4.jpg"
+  },
+  {
+    "revision": "58ef20d042a7a04fd92e0793eab781d0",
+    "url": "/static/media/Boruto.58ef20d0.jpg"
+  },
+  {
+    "revision": "a2dadff0f9359735827acfee5fd794f1",
+    "url": "/static/media/Boruto.a2dadff0.mp4"
+  },
+  {
+    "revision": "2728075b98bcd7edd045389730ff5d0d",
+    "url": "/static/media/EdsHeinz.2728075b.mp4"
+  },
+  {
+    "revision": "8d9ad3ebbadd2427b645bc3d1cf69aed",
+    "url": "/static/media/EdsHeinz.8d9ad3eb.jpg"
+  },
+  {
+    "revision": "2eca36b99d27498bfcbf5d0bfa890567",
+    "url": "/static/media/EmployeeOfTheMonth.2eca36b9.mp4"
+  },
+  {
+    "revision": "8cc8a24acfd6fe122ffe004a959bc14d",
+    "url": "/static/media/EmployeeOfTheMonth.8cc8a24a.jpg"
+  },
+  {
+    "revision": "7220207ec5881d89b2aef3d1ea6c5b75",
+    "url": "/static/media/Exile.7220207e.mp4"
+  },
+  {
+    "revision": "f9a3ba2ba52395d7a533e3dc7aecfe3b",
+    "url": "/static/media/Exile.f9a3ba2b.jpg"
+  },
+  {
+    "revision": "120ee5475f2431e72a1895845969c324",
+    "url": "/static/media/Extraordinary.120ee547.jpg"
+  },
+  {
+    "revision": "947e0c3929efcd28afe9fd8d4858f5ee",
+    "url": "/static/media/Extraordinary.947e0c39.mp4"
+  },
+  {
+    "revision": "0e2d670519acb81bb770520ad6b5e17b",
+    "url": "/static/media/From.0e2d6705.mp4"
+  },
+  {
+    "revision": "04c8011c53bbd0cd0e26231e098ccc39",
+    "url": "/static/media/HarryPotterandtheChamberofSecrets.04c8011c.mp4"
+  },
+  {
+    "revision": "faf9f765d93c8850faeee37cf8ac2594",
+    "url": "/static/media/HarryPotterandtheChamberofSecrets.faf9f765.jpg"
+  },
+  {
+    "revision": "677810b2685e032b983df00728b12c50",
+    "url": "/static/media/Hunchback.677810b2.jpg"
+  },
+  {
+    "revision": "89b33799e54186418103140613ee37e9",
+    "url": "/static/media/Hunchback.89b33799.mp4"
+  },
+  {
+    "revision": "b763a5e6fddee8689eca7acb3a10804b",
+    "url": "/static/media/Joker.b763a5e6.jpg"
+  },
+  {
+    "revision": "c738e3d9ef1590aad47a94b76dca20c8",
+    "url": "/static/media/Joker.c738e3d9.mp4"
+  },
+  {
+    "revision": "819c7da401be73c71fd5ed6f7ae0697c",
+    "url": "/static/media/MobileSuitGundam.819c7da4.jpg"
+  },
+  {
+    "revision": "d456126bb1d2b3c19ff15a493a399a0b",
+    "url": "/static/media/MobileSuitGundam.d456126b.mp4"
+  },
+  {
+    "revision": "597906a0f30161ee4b1874d513da88a6",
+    "url": "/static/media/NOISES.597906a0.mp4"
+  },
+  {
+    "revision": "91bb8b7ed6fb1d652fb07a544f1ed034",
+    "url": "/static/media/NOISES.91bb8b7e.jpg"
+  },
+  {
+    "revision": "aca015e5bffe48c65ddcc0fb13ba2c62",
+    "url": "/static/media/OneYou.aca015e5.jpg"
+  },
+  {
+    "revision": "bf6ef3da2497784f56388126e396c4be",
+    "url": "/static/media/OneYou.bf6ef3da.mp4"
+  },
+  {
+    "revision": "9b48b0d41554e4301dee6fbdfebf2aaa",
+    "url": "/static/media/Ponyo.9b48b0d4.jpg"
+  },
+  {
+    "revision": "f9edc301fbb80396b3c24d10bc2126bf",
+    "url": "/static/media/Ponyo.f9edc301.mp4"
+  },
+  {
+    "revision": "8e4626c418c5833f8cc459cf53ecdceb",
+    "url": "/static/media/RockyV.8e4626c4.mp4"
+  },
+  {
+    "revision": "a8141903bbfa350bcbc34584abe62c71",
+    "url": "/static/media/RockyV.a8141903.jpg"
+  },
+  {
+    "revision": "7c1c8a9365b1fb6deb23cc1b289abece",
+    "url": "/static/media/StarWarsEpisodeVIIITheLastJedi.7c1c8a93.mp4"
+  },
+  {
+    "revision": "e3f383caecedc0222e68dd6cdc9f9b9c",
+    "url": "/static/media/StarWarsEpisodeVIIITheLastJedi.e3f383ca.jpg"
+  },
+  {
+    "revision": "372d667bb921267ca8bf9832ad255301",
+    "url": "/static/media/THEAUTOFIRMWITHALEXVEGA.372d667b.jpg"
+  },
+  {
+    "revision": "606d8212bd42b811f84d6e455dab3396",
+    "url": "/static/media/THEAUTOFIRMWITHALEXVEGA.606d8212.mp4"
+  },
+  {
+    "revision": "22534f3b7df9348b10e22c6ed016a777",
+    "url": "/static/media/THELEGO®MOVIE™4DANEWADVENTURE.22534f3b.jpg"
+  },
+  {
+    "revision": "f8136e90f680bf7510a0923092f1ac84",
+    "url": "/static/media/THELEGO®MOVIE™4DANEWADVENTURE.f8136e90.mp4"
+  },
+  {
+    "revision": "1f77c97b48246e9d50e26177526dc654",
+    "url": "/static/media/TheBadGuys.1f77c97b.mp4"
+  },
+  {
+    "revision": "61fb5f7cdba44d819579e1b70e18c894",
+    "url": "/static/media/TheBadGuys.61fb5f7c.jpg"
+  },
+  {
+    "revision": "61773f932273dc9e523e49d68158c552",
+    "url": "/static/media/WeBareBears.61773f93.mp4"
+  },
+  {
+    "revision": "b0d907a65a418c15d33ffb3090e6807c",
+    "url": "/static/media/WeBareBears.b0d907a6.jpg"
+  },
+  {
+    "revision": "25f624df494a36f73d3cc015047716a0",
+    "url": "/static/media/dashicon.25f624df.png"
+  },
+  {
+    "revision": "ef12b2db177389a93baae33f6e907087",
+    "url": "/static/media/developer1.ef12b2db.jpg"
+  },
+  {
+    "revision": "d6e2a6fc515d439d3fd6fc868e696f96",
+    "url": "/static/media/developer2.d6e2a6fc.jpg"
+  },
+  {
+    "revision": "f593a6a86a8e16311bb7d3223c26f0e7",
+    "url": "/static/media/developer3.f593a6a8.jpg"
+  },
+  {
+    "revision": "d0e0b025dd5ed0ce74cb312113c1a312",
+    "url": "/static/media/homepage.d0e0b025.jpg"
+  },
+  {
+    "revision": "6dfa3771efd69b37902f723323658152",
+    "url": "/static/media/login.6dfa3771.jpg"
+  },
+  {
+    "revision": "fbc874be7379e285dc13dfb8473ae7bc",
+    "url": "/static/media/registration.fbc874be.jpg"
+  },
+  {
+    "revision": "c9d8721ccea9ac4049e4f621f8cc2a56",
+    "url": "/static/media/searchicon.c9d8721c.png"
+  },
+  {
+    "revision": "119e824bbfa3dead702956fb70f17ae5",
+    "url": "/static/media/subscription.119e824b.jpg"
+  },
+  {
+    "revision": "d4baa525810a7cd60ef310a737f59f46",
+    "url": "/static/media/usericon.d4baa525.png"
+  },
+  {
+    "revision": "4e39c6d225c072823d8d00edb0b7c2d0",
+    "url": "/static/media/usersettings.4e39c6d2.jpg"
+  }
+]);
